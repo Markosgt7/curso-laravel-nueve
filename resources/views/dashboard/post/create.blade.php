7bfd92e2a29ab2ccbac41fa328f4e16272746f64
Marcos Joél Sánchez López <mjsanchezl@mineco.gob.gt>
2022-12-07 15:27:29 -0600

    65 crear layout para administrativo

--- resources/views/dashboard/post/create.blade.php
+++ resources/views/dashboard/post/create.blade.php
@@ -1,44 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Crear Post</title>
-</head>
-<body>
-    <h1>Crear post</h1>
-      @include('dashboard.fragment._errors-form')
-    <form action="{{ route('post.store')}}" method="post">
-    @csrf
-    <label for="title">Título
+@extends('dashboard.layout');
+@section('content')
+        <h1>Crear post</h1>
+        @include('dashboard.fragment._errors-form')
+        <form action="{{ route('post.store')}}" method="post">
+        @csrf
+        <label for="title">Título
         <input type="text" name="title" >
-    </label>
-    <label for="title">Slug
+        </label>
+        <label for="title">Slug
         <input type="text" name="slug" >
-    </label>
-    <label for="category">Categoría
+        </label>
+        <label for="category">Categoría
         <select name="category_id">
             <option value="uno">Seleccione</option>
             @foreach ($categories as $title=>$id)
                 <option value="{{$id}}">{{ $title }}</option>
             @endforeach
         </select>
-    </label>
-    <label for="posted">Posteado
+        </label>
+        <label for="posted">Posteado
         <select name="posted">
             <option value="not">No</option>
             <option value="yes">Yes</option>
         </select>
-    </label>
-    <label for="title">Contenido
+        </label>
+        <label for="title">Contenido
         <textarea name="content"></textarea>
-    </label>
-    <label for="title">Descripción
+        </label>
+        <label for="title">Descripción
         <input type="text" name="description" >
-    </label>
-    <button type="submit"> Enviar</button>
-    </form>
+        </label>
+        <button type="submit"> Enviar</button>
+        </form>
     
-</body>
-</html>
+@endsection
