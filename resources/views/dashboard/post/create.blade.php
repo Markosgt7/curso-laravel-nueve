c078f98ee8948e7d73f9a968fa5f7930d42f12e0
Marcos Joél Sánchez López <mjsanchezl@mineco.gob.gt>
2022-12-08 11:35:14 -0600

    73 crear valor anterior funcion old

--- resources/views/dashboard/post/create.blade.php
+++ resources/views/dashboard/post/create.blade.php
@@ -5,30 +5,30 @@
         <form action="{{ route('post.store')}}" method="post">
         @csrf
         <label for="title">Título
-        <input type="text" name="title" >
+        <input type="text" name="title" value="{{ old('title', '') }}">
         </label>
         <label for="title">Slug
-        <input type="text" name="slug" >
+        <input type="text" name="slug" value="{{ old('slug', '') }}">
         </label>
         <label for="category">Categoría
         <select name="category_id">
             <option value="uno">Seleccione</option>
             @foreach ($categories as $title=>$id)
-                <option value="{{$id}}">{{ $title }}</option>
+                <option {{ old('category_id','')== $id ? 'selected': '' }} value="{{$id}}">{{ $title }}</option>
             @endforeach
         </select>
         </label>
         <label for="posted">Posteado
         <select name="posted">
-            <option value="not">No</option>
-            <option value="yes">Yes</option>
+            <option {{ old('posted', '')  == 'not' ? 'selected' : ''}}  value="not">No</option>
+            <option {{ old('posted', '')  == 'yes' ? 'selected' : ''}}  value="yes">Yes</option>
         </select>
         </label>
         <label for="title">Contenido
-        <textarea name="content"></textarea>
+        <textarea name="content">{{ old('title', '') }}</textarea>
         </label>
         <label for="title">Descripción
-        <input type="text" name="description" >
+        <textarea type="text" name="description" > {{ old('title', '') }}</textarea>
         </label>
         <button type="submit"> Enviar</button>
         </form>
